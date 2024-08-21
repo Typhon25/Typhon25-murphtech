cf92d170c28fdc5aef3d7c0b9b4320fe9fd01cd9
Typhon25 <abhinavchouhan117@gmail.com>
2024-08-21 23:45:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/courses', (req, res) => {
+    res.render('courses');
+});
+
+
+
+app.listen(3000)
