031822d5d63ef762fc5f6dff5673a65bd6e5e607
Typhon25 <abhinavchouhan117@gmail.com>
2024-08-23 17:20:51 +0530

    Update styles, fonts, and scripts; Modify index
    - Updated style.css to enhance UI.
    - Added SFProDisplay-Bold font for better typography.
    - Implemented new JavaScript features in script.js.
    - Modified courses.ejs and index.ejs views for improved layout and functionality.

---
+++ public/javascript/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const img = document.querySelector('.img-1');
+    if (img) {
+        img.addEventListener('click', () => {
+            window.location.href = '/'; 
+        });
+    }
+
+    const button = document.querySelector('.btn-course');
+    if (button) {
+        button.addEventListener('click', () => {
+            window.location.href = '/courses'; 
+        });
+    }
+});
